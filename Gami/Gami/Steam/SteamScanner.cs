7597d545690a6f71c9385fd47f2c9b4ef90b5e6f
Tom Bebbington <tophattedcoder@gmail.com>
2024-08-07 11:44:37 +0100

    Scan steam manifests for more accurate results

--- Gami/Gami/Steam/SteamScanner.cs
+++ Gami/Gami/Steam/SteamScanner.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Frozen;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using Gami.Base;
 using Gami.Db.Models;
-using Instances;
 using Serilog;
+using ValveKeyValue;
 
 namespace Gami.Scanners;
 
@@ -37,9 +33,44 @@ public sealed class SteamScanner : IGameLibraryScanner
     public static readonly string AppListCachePath = Path.Join(App.AppDir, "steam_cache.json");
 
     private static string ScanPath => OperatingSystem.IsWindows()
-        ? @"C:\Program Files (x86)\Steam\steamapps\shadercache"
+        ? @"C:\Program Files (x86)\Steam\steamapps"
         : Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-            "Steam/steamapps/compatdata");
+            "Steam/steamapps");
+
+    private static GameLibraryRef MapGameManifest(string path)
+    {
+        Log.Debug("MapGameMan {Path}", path);
+        var stream = File.OpenRead(path);
+        Log.Debug("MapGame Opened stream {Path}", path);
+        var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+        Log.Debug("MapGame created deserializer {Path}", path);
+        KVObject data = kv.Deserialize(stream);
+        Log.Debug("MapGame deserialized {Path}", path);
+        var appId = data["appid"]?.ToString();
+        Log.Debug("Raw appId: {AppId}", appId);
+        var name = data["name"].ToString();
+
+        Log.Debug("Raw name: {AppId}", name);
+        var bytesToDl = data["BytesToDownload"].ToString();
+        Log.Debug("Raw BytesToDownload: {AppId}", bytesToDl);
+
+        var bytesDl = data["BytesDownloaded"].ToString();
+        Log.Debug("Raw BytesDownloaded: {AppId}", bytesDl);
+
+        var mapped = new GameLibraryRef()
+        {
+            LibraryType = "Steam",
+            LibraryId = appId,
+            Name = name,
+            InstallStatus = bytesDl == bytesToDl
+                ? GameInstallStatus.Installed
+                : GameInstallStatus.Installing
+        };
+
+        Log.Debug("Mapped bytes: {Mapped}", JsonSerializer.Serialize(mapped));
+        return mapped;
+    }
+
 
     public async IAsyncEnumerable<IGameLibraryRef> Scan()
     {
@@ -50,49 +81,18 @@ public sealed class SteamScanner : IGameLibraryScanner
             yield break;
         }
 
-        var paths = OperatingSystem.IsWindows()
-            ? Directory.EnumerateFiles(path)
-            : Directory.EnumerateDirectories(path).Except(new[] { "0" });
-        var appListRes = await GetAppList();
-        var appListById = appListRes?.AppList.Apps.DistinctBy(k => k.AppId).ToFrozenDictionary(k => k.AppId) ??
-                          FrozenDictionary<int, AppListItem>.Empty;
-        if (appListById.Count == 0)
-
-            throw new ApplicationException("Invalid app list");
-        foreach (var file in paths)
+        Log.Debug("Scanning steam games in {Dir}", path);
+        foreach (var partialPath in Directory.GetFiles(path, @"appmanifest*"))
         {
-            var appId = Path.GetFileName(file);
-            var name = appListById.GetValueOrDefault(int.Parse(appId))?.Name;
-            if (name == null)
+            var manifestPath = Path.Combine(path, partialPath);
+            Log.Debug("Mapping game manifest at {Path}", manifestPath);
+            var mapped = MapGameManifest(manifestPath);
+            Log.Debug("Mapped game manifest at {Path}", manifestPath);
+            var name = mapped.Name;
+            if (name == "Steam Controller Configs" || name.StartsWith("Steam Linux") || name.StartsWith("Proton") ||
+                name.StartsWith("Steamworks"))
                 continue;
-            yield return new GameLibraryRef
-            {
-                LibraryId = appId,
-                LibraryType = "steam",
-                Name = name
-            };
+            yield return mapped;
         }
-    }
-
-    public static async ValueTask<AppListResult?> GetAppList()
-    {
-        var hasCacheFile = Path.Exists(AppListCachePath);
-        var sinceLastDownload =
-            hasCacheFile ? DateTime.UtcNow - File.GetLastWriteTimeUtc(AppListCachePath) : TimeSpan.Zero;
-        Log.Debug("Get App List; hasCache={HasCache}, sinceLastDl={SinceLastDownload}", hasCacheFile,
-            sinceLastDownload);
-        if (!hasCacheFile || sinceLastDownload > TimeSpan.FromMinutes(30))
-        {
-            Log.Debug("Fetching app list");
-            await new ProcessStartInfo("curl",
-                $"https://api.steampowered.com/ISteamApps/GetAppList/v2 -o {AppListCachePath}").RunAsync();
-            Log.Debug("Fetched app list");
-        }
-
-        Log.Debug("Reading from cache: {CachePath}", AppListCachePath);
-
-        var text = File.ReadAllText(AppListCachePath);
-        Log.Debug("Got App List");
-        return JsonSerializer.Deserialize<AppListResult>(text, SerializerSettings.JsonOptions);
     }
 }
