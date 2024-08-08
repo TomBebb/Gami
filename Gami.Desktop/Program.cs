1d64b52da4d774e9a5306ad464700067687b9a1f
Tom Bebbington <tombebb@protonmail.com>
2024-08-08 02:57:24 +0100

    Relocate desktop folder

--- Gami.Desktop/Program.cs
+++ Gami.Desktop/Program.cs
@@ -25,12 +25,10 @@ internal sealed class Program
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-    {
-        return AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
-    }
 }
