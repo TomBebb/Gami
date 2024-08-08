392fce3dd17aa3628b812b755e0352bece6d1b0f
Tom Bebbington <tombebb@protonmail.com>
2024-08-08 23:33:00 +0100

    Support plugin scanning

--- Gami.Desktop/Plugins/GameExtensions.cs
+++ Gami.Desktop/Plugins/GameExtensions.cs
@@ -3,6 +3,10 @@ using System.Collections.Frozen;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Avalonia.Controls.Documents;
 using Gami.Core;
 using Gami.Core.Models;
 using Serilog;
@@ -62,6 +66,38 @@ public static class GameExtensions
         "C:\\Users\\topha\\Code\\Gami\\Gami.Scanner.Steam\\bin\\Debug\\net8.0\\Gami.Scanner.Steam.dll"
     };
 
+    private static JsonSerializerOptions _pluginOpts = new(JsonSerializerDefaults.Web)
+    {
+        Converters =
+        {
+            new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+        },
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    private static PluginConfig LoadPluginConfig(Assembly assembly)
+    {
+        var manifest = assembly.GetManifestResourceNames();
+        var configPath = $"{assembly.FullName!.Split(",")[0]}.config.json";
+        Log.Debug("Manifest for {Assembly}; {Games}; Config={Conf}", assembly.FullName, manifest, configPath);
+        var configStream = assembly.GetManifestResourceStream(configPath);
+
+        if (configStream == null)
+            throw new ApplicationException($"Missing '{configPath}' in {assembly.FullName!}");
+
+        Log.Debug("Parse test: {Parse}", JsonSerializer.Deserialize<PluginSettingType>("\"string\"", _pluginOpts));
+/*
+        var configTextStream = new MemoryStream();
+        configStream.CopyTo(configTextStream);
+
+        var configText = Encoding.UTF8.GetString(configTextStream.GetBuffer());
+        if (configText[0] == '?')
+            configText = configText.Substring(1);
+
+        Log.Debug("Config.json: {Content}; First: {First}", configText, configText[0]);
+*/
+        return JsonSerializer.Deserialize<PluginConfig>(configStream, _pluginOpts);
+    }
 
     private static readonly FrozenDictionary<string, IGameLibraryManagement>
         InstallersByName = Plugins.Select(p =>
@@ -95,6 +131,14 @@ public static class GameExtensions
                 return GetMatching<IGameAchievementScanner>(assembly);
             })
             .ToFrozenDictionary(v => v.Type);
+
+    public static readonly FrozenDictionary<string, PluginConfig>
+        PluginConfigs = Plugins.Select(p =>
+            {
+                var assembly = LoadPlugin(p);
+                return (p, LoadPluginConfig(assembly));
+            })
+            .ToFrozenDictionary(v => v.p, v => v.Item2);
 
     private static T GetLauncher<T>(this FrozenDictionary<string, T> dictionary,
         string libType)
---
+++ Gami.Desktop/Plugins/PluginConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace Gami.Desktop.Plugins;
+
+public enum PluginSettingType
+{
+    String,
+    Int,
+    Bool
+}
+
+public sealed class PluginValueConfig
+{
+    public required string Key { get; set; }
+    public required string Name { get; set; }
+
+    public PluginSettingType Type { get; set; } = PluginSettingType.String;
+}
+
+public sealed class PluginConfig
+{
+    public required string Key { get; set; }
+    public required string Name { get; set; }
+}
