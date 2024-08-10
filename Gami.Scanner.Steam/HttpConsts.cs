574087d845de6f35412c4fe94dc98fdcf3af5b3f
Tom Bebbington <tombebb@protonmail.com>
2024-08-10 01:54:08 +0100

    Revert "Cache http"
    This reverts commit d41ccf711574e9f6304961662e861bf3e5385b65.

--- Gami.Scanner.Steam/HttpConsts.cs
+++ Gami.Scanner.Steam/HttpConsts.cs
@@ -1,8 +1,9 @@
-﻿using CacheCow.Client;
-
-namespace Gami.Scanner.Steam;
+﻿namespace Gami.Scanner.Steam;
 
 public static class HttpConsts
 {
-    public static readonly HttpClient HttpClient = ClientExtensions.CreateClient();
+    public static readonly HttpClient HttpClient = new(new HttpClientHandler
+    {
+        MaxConnectionsPerServer = 8
+    });
 }
