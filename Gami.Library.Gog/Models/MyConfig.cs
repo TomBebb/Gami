9600c6510824042e1f273826d235442bef38a17b
Tom Bebbington <tombebb@protonmail.com>
2024-08-13 23:05:58 +0100

    GOG: Remove unused installedgames conf

--- Gami.Library.Gog/Models/MyConfig.cs
+++ Gami.Library.Gog/Models/MyConfig.cs
@@ -1,13 +1,8 @@
-﻿using System.Collections.Immutable;
-
-namespace Gami.Library.Gog.Models;
+﻿namespace Gami.Library.Gog.Models;
 
 public sealed class MyConfig
 {
     public string AccessToken { get; set; } = "";
     public string RefreshToken { get; set; } = "";
     public DateTime? AccessTokenExpire { get; set; }
-
-
-    public ImmutableDictionary<long, string> InstalledGames { get; set; } = ImmutableDictionary<long, string>.Empty;
 }
