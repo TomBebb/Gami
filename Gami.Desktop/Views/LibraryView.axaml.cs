72814ed4370770098450786730f51d5e971aa66a
Tom Bebbington <tombebb@protonmail.com>
2024-09-25 22:27:11 +0100

    Separate main view and game library view

---
+++ Gami.Desktop/Views/LibraryView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Gami.Desktop.Views;
+
+public partial class LibraryView : UserControl
+{
+    public LibraryView()
+    {
+        InitializeComponent();
+    }
+}
