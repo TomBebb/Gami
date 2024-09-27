0bd012e6484a82302e48c49b9843bb60fa377235
Tom Bebbington <tombebb@protonmail.com>
2024-09-27 23:57:04 +0100

    Use icons for game status display

---
+++ Gami.Desktop/Conv/InstallStatusToIconConv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using FluentAvalonia.UI.Controls;
+using Gami.Core.Models;
+
+namespace Gami.Desktop.Conv;
+
+public class InstallStatusToIconConv : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (!(value is GameInstallStatus status))
+            throw new ArgumentException(nameof(value));
+
+        return status switch
+        {
+            GameInstallStatus.Installing => Symbol.Rotate,
+            GameInstallStatus.Queued => Symbol.Rotate,
+            GameInstallStatus.Installed => Symbol.Checkmark,
+            GameInstallStatus.InLibrary => Symbol.Clear
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
