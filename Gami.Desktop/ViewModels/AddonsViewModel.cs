d46d62a6e1e53217de545a7b7ef6882cd5f7669a
Tom Bebbington <tombebb@protonmail.com>
2024-09-27 01:07:30 +0100

    Support plugin selection

--- Gami.Desktop/ViewModels/AddonsViewModel.cs
+++ Gami.Desktop/ViewModels/AddonsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Immutable;
 using System.Linq;
 using Gami.Desktop.Plugins;
+using ReactiveUI.Fody.Helpers;
 using Serilog;
 
 namespace Gami.Desktop.ViewModels;
@@ -10,7 +11,11 @@ public sealed class AddonsViewModel : ViewModelBase
     public AddonsViewModel()
     {
         Log.Information("Installed addons: {Addons}", Installed.Select(addon => addon.Name));
+
+        SelectedAddon = Installed.First();
     }
 
     public ImmutableArray<PluginConfig> Installed => GameExtensions.PluginConfigs.Values;
+
+    [Reactive] public PluginConfig SelectedAddon { get; set; }
 }
