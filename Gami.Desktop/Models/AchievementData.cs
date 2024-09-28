bf241f912840df1ec573a9d4785b09a520ed6e29
Tom Bebbington <tombebb@protonmail.com>
2024-09-28 14:46:28 +0100

    Display achievements icon

--- Gami.Desktop/Models/AchievementData.cs
+++ Gami.Desktop/Models/AchievementData.cs
@@ -2,4 +2,7 @@
 
 namespace Gami.Desktop.Models;
 
-public record struct AchievementData(Achievement Achievement, AchievementProgress Progress);
+public record struct AchievementData(Achievement Achievement, AchievementProgress Progress)
+{
+    public string IconUrl => Progress.Unlocked ? Achievement.UnlockedIconUrl : Achievement.LockedIconUrl;
+}
