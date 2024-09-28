918c4056b413743bb91a1a91dd2c6f76c29e4151
Tom Bebbington <tombebb@protonmail.com>
2024-09-28 16:07:31 +0100

    Support achievements filtering

--- Gami.Desktop/ViewModels/AchievementsViewModel.cs
+++ Gami.Desktop/ViewModels/AchievementsViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.Linq;
 using Gami.Core.Models;
 using Gami.Desktop.Db;
 using Gami.Desktop.Models;
+using Humanizer;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using Serilog;
@@ -20,27 +22,39 @@ public class AchievementsViewModel : ViewModelBase
             Games = [..db.Games.Select(g => new Game { Id = g.Id, Name = g.Name, LibraryId = g.LibraryId })];
             SelectedGame = Games.FirstOrDefault();
         }
+
+        this.WhenAnyValue(v => v.Filter).Subscribe(v => { ReloadAchievements(); });
     }
 
     [Reactive] public ImmutableArray<Game> Games { get; set; }
+
+    [Reactive] public AchievementsFilter Filter { get; set; }
+
+    public string[] FilterOptions => Enum.GetValues<AchievementsFilter>().Select(af => af.Humanize()).ToArray();
 
     public Game? SelectedGame
     {
         get => _selectedGame;
         set
         {
-            Log.Debug("Selected game changed! Fetching achievements");
-            using var db = new GamiContext();
-
-            Achievements = db.Achievements.AsQueryable().Where(a => a.GameId == value.Id)
-                .Select(a => new AchievementData(a, a.Progress ?? new AchievementProgress()))
-                .ToImmutableArray();
-            Log.Debug("Selected game changed! Fetched achievements");
-
             this.RaiseAndSetIfChanged(ref _selectedGame, value);
+            ReloadAchievements();
         }
     }
 
     [Reactive]
     public ImmutableArray<AchievementData> Achievements { get; set; } = ImmutableArray<AchievementData>.Empty;
+
+    private void ReloadAchievements()
+    {
+        Log.Debug("Selected game changed! Fetching achievements");
+        using var db = new GamiContext();
+
+        Achievements = db.Achievements.AsQueryable().Where(a => a.GameId == SelectedGame.Id)
+            .Where(a => Filter == AchievementsFilter.None ||
+                        Filter == AchievementsFilter.Locked == !a.Progress.Unlocked)
+            .Select(a => new AchievementData(a, a.Progress ?? new AchievementProgress()))
+            .ToImmutableArray();
+        Log.Debug("Selected game changed! Fetched achievements");
+    }
 }
