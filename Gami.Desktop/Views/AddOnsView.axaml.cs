5bf08fb2bc8d1e972002752fa14fcc14446f456a
Tom Bebbington <tombebb@protonmail.com>
2024-10-01 22:32:03 +0100

    Fix bug #53 & #52: addon  configs fixes

--- Gami.Desktop/Views/AddOnsView.axaml.cs
+++ Gami.Desktop/Views/AddOnsView.axaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System.Collections.Generic;
+using Avalonia.Controls;
+using Gami.Core;
+using Gami.Desktop.Plugins;
+using Gami.Desktop.ViewModels;
 
 namespace Gami.Desktop.Views;
 
@@ -7,5 +11,16 @@ public partial class AddOnsView : UserControl
     public AddOnsView()
     {
         InitializeComponent();
+    }
+
+    private void TextBox_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        var model = (MappedPluginConfigSetting)((TextBox)sender).DataContext;
+
+        var addons = (AddonsViewModel)((TextBox)sender)?.Parent?.Parent?.Parent?.Parent?.DataContext;
+        var addon = addons.SelectedAddon;
+        var curr = PluginJson.Load<Dictionary<string, object>>(addon.Key) ?? new Dictionary<string, object>();
+        curr[model!.Key] = model!.Value;
+        PluginJson.Save(curr, addon.Key);
     }
 }
