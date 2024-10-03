20372f22edc5fc4d6226c5dff9f6f2835d3c9209
Tom Bebbington <tombebb@protonmail.com>
2024-10-03 21:34:27 +0100

    Support right-click menu

--- Gami.Desktop/Views/LibraryView.axaml.cs
+++ Gami.Desktop/Views/LibraryView.axaml.cs
@@ -1,4 +1,7 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Serilog;
 
 namespace Gami.Desktop.Views;
 
@@ -7,5 +10,19 @@ public partial class LibraryView : UserControl
     public LibraryView()
     {
         InitializeComponent();
+
+        var box = this.FindLogicalDescendantOfType<ListBox>();
+        box.AddHandler(PointerPressedEvent, (sender, args) =>
+        {
+            Log.Information("Pressed PointerPressedEvent");
+            var point = args.GetCurrentPoint(sender as Control);
+            var props = point.Properties;
+            Log.Information("Pressed PointerPressedEvent: {ty}",
+                props.IsLeftButtonPressed ? "Left" : props.IsRightButtonPressed ? "Right" : "Middle");
+            if (!props.IsLeftButtonPressed)
+                args.Handled = true;
+        }, RoutingStrategies.Tunnel);
+
+        Log.Debug("Root Grid: {Grid}", box);
     }
 }
