e24ae15d6290883b29b78c6d1033b45da8d056b5
Tom Bebbington <tombebb@protonmail.com>
2024-10-04 21:53:47 +0100

    Combine title bar and title bar

--- Gami.Desktop/Views/MainWindow.axaml.cs
+++ Gami.Desktop/Views/MainWindow.axaml.cs
@@ -1,11 +1,15 @@
 using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
 
 namespace Gami.Desktop.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : AppWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex; 
+        
     }
 }
