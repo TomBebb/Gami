c790f8d7c0393c1e742d4131975f6e220bffcff5
Tom D Bebbington <tombebb@protonmail.com>
2024-11-14 15:11:43 +0000

    Show time in big picture

--- Gami.BigPicture/ViewModels/MainWindowViewModel.cs
+++ Gami.BigPicture/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 using Gami.BigPicture.Views;
 using ReactiveUI.Fody.Helpers;
 
@@ -9,7 +11,18 @@ public class MainWindowViewModel : ViewModelBase
     public MainWindowViewModel()
     {
         CurrView = new LibraryView { DataContext = CurrViewModel };
+
+        Task.Run(async () =>
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMinutes(1));
+                CurrentTime.Add(TimeSpan.FromMinutes(1));
+            }
+        });
     }
+
+    [Reactive] public TimeOnly CurrentTime { get; set; } = TimeOnly.FromDateTime(DateTime.Now);
 
     [Reactive] public ViewModelBase? CurrViewModel { get; set; } = new LibraryViewModel();
     [Reactive] public UserControl? CurrView { get; set; }
