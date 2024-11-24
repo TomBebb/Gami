d4688639bff0625d5e66b2812b9a9d46eb6fcf2f
Tom D Bebbington <tombebb@protonmail.com>
2024-11-24 23:13:07 +0000

    Add basic window buttons

--- Gami.Desktop/Views/MainView.axaml.cs
+++ Gami.Desktop/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using FluentAvalonia.UI.Controls;
 using Gami.Desktop.ViewModels;
 using Serilog;
@@ -15,7 +16,24 @@ public partial class MainView : UserControl
     private void NavigationView_OnSelectionChanged(object? sender, NavigationViewSelectionChangedEventArgs e)
     {
         Log.Debug("NavigationView_OnSelectionChanged: {Selection}", e.SelectedItem);
-        (DataContext as MainViewModel)!.CurrRoute = (Route)e.SelectedItem ;
+        (DataContext as MainViewModel)!.CurrRoute = (Route)e.SelectedItem;
+    }
 
+    private void Minimize(object? sender, RoutedEventArgs e)
+    {
+        var window = (Window)Parent;
+        window!.WindowState = WindowState.Minimized;
+    }
+
+    private void Maximize(object? sender, RoutedEventArgs e)
+    {
+        var window = (Window)Parent;
+        window!.WindowState = WindowState.Maximized;
+    }
+
+    private void Close(object? sender, RoutedEventArgs e)
+    {
+        var window = (Window)Parent;
+        window!.Close();
     }
 }
