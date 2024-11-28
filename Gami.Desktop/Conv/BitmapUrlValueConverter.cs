a9eacf7628e621fd7affd77e9557d5ff25501284
Tom D Bebbington <tombebb@protonmail.com>
2024-11-28 02:20:22 +0000

    Tidy

--- Gami.Desktop/Conv/BitmapUrlValueConverter.cs
+++ Gami.Desktop/Conv/BitmapUrlValueConverter.cs
@@ -13,8 +13,7 @@ public class BitmapValueConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value == null)
-            throw new ArgumentNullException(nameof(value));
+        ArgumentNullException.ThrowIfNull(value);
         if (targetType != typeof(IImage))
             throw new NotSupportedException();
 
